123d617439e75b6ba17f2c98537aadbdd3830829
Saurav1999 <35275359+Saurav1999@users.noreply.github.com>
2019-02-12 08:17:03 +0530

    Add files via upload

---
+++ Dynamic/LIS(Longest Increasing sequence  ).py
@@ -0,0 +1,9 @@
+def longest_increasing_subsequence(arr):
+    if not arr:
+        return 0
+    cache = [1] * len(arr)
+    for i in range(1, len(arr)):
+        for j in range(i):
+            if arr[i] > arr[j]:
+                cache[i] = max(cache[i], cache[j] + 1)
+    return max(cache)
